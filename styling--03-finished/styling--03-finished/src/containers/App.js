6d106a25bd9778fac79cdc84b52b34149c7bd05f
vlkrrrr <yakityyakyak@outlook.com>
2020-09-01 15:31:24 +0200

    deep dive

--- styling--03-finished/styling--03-finished/src/containers/App.js
+++ styling--03-finished/styling--03-finished/src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
+import C
 
 class App extends Component {
   state = {
@@ -47,8 +48,6 @@ class App extends Component {
 
   render() {
     let persons = null;
-    let btnClass = '';
-
     if (this.state.showPersons) {
       persons = (
         <div>
@@ -59,24 +58,11 @@ class App extends Component {
         </div>
       );
 
-      btnClass = classes.Red;
-    }
 
-    const assignedClasses = [];
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.red); // classes = ['red']
-    }
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold); // classes = ['red', 'bold']
     }
 
     return (
       <div className={classes.App}>
-        <h1>Hi, I'm a React App</h1>
-        <p className={assignedClasses.join(' ')}>This is really working!</p>
-        <button className={btnClass} onClick={this.togglePersonsHandler}>
-          Toggle Persons
-        </button>
         {persons}
       </div>
     );
